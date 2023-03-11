15804ce85d439fc499e08017d7fe616a8411ddb6
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2023-03-10 22:16:49 -0300

    FEAT: Caixa de diálogo para informar o valor a ser convertido;

--- Converter.java
+++ Converter.java
@@ -4,10 +4,21 @@ public class Converter {
     public static void main(String[] args) {
         String[] opcoes = {"Dólar para Euro", "Dólar para Real", "Euro para Dólar", "Euro para Real",
                 "Real para Dólar", "Real para Euro"};
+
         String input = (String) JOptionPane.showInputDialog(null, "Esolha uma opção", "Conversor de moedas",
                 JOptionPane.QUESTION_MESSAGE, null,
                 opcoes,
                 opcoes[1]);
-        System.out.println(input);
+
+        if(input != null){
+            String valor = JOptionPane.showInputDialog(null, "Digite o valor que deseja converter:", "Conversor de moedas", JOptionPane.PLAIN_MESSAGE);
+        }
+        if(input !=  null && !input.isEmpty()){
+            double valor = Double.parseDouble(input);
+
+        }else{
+            JOptionPane.showMessageDialog(null, "Valor inválido", "Conversor de moedas", JOptionPane.ERROR_MESSAGE);
+        }
+
     }
 }
